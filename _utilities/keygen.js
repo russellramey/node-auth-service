680a3cafcd7f19a00bbfb034b2d4fb672dfeb01d
Russell Ramey <doublercreative@gmail.com>
2022-01-19 15:24:56 -0600

    changed default name of rsa key files

--- _utilities/keygen.js
+++ _utilities/keygen.js
@@ -9,22 +9,22 @@ const fs = require('fs');
 /**
 *
 * Generate Private/Public key pair
-* 
+*
 * Generates keypair to use to sign and verify JWTs
 * issued by this service
 *
 **/
 function generateKeyPair() {
-    
+
     // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
     const keyPair = crypto.generateKeyPairSync('rsa', {
         // bits - standard for RSA keys
-        modulusLength: 4096, 
+        modulusLength: 4096,
         publicKeyEncoding: {
             // type: pkcs1 - Public Key Cryptography Standards 1
-            type: 'pkcs1',  
+            type: 'pkcs1',
             // format: .pem - most common formatting choice
-            format: 'pem' 
+            format: 'pem'
         },
         privateKeyEncoding: {
             type: 'pkcs1',
@@ -33,10 +33,10 @@ function generateKeyPair() {
     });
 
     // Create the public key file, save to root
-    fs.writeFileSync(__dirname + '/../id_rsa_pub.pem', keyPair.publicKey); 
-    
+    fs.writeFileSync(__dirname + '/../rsa_key_pub.pem', keyPair.publicKey);
+
     // Create the private key file, save to root
-    fs.writeFileSync(__dirname + '/../id_rsa_priv.pem', keyPair.privateKey);
+    fs.writeFileSync(__dirname + '/../rsa_key_priv.pem', keyPair.privateKey);
 
 }
 
