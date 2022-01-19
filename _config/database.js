fd8f2745eaf9670757ec227e2d61eea1da7dc8b1
Russell Ramey <doublercreative@gmail.com>
2022-01-19 13:33:43 -0600

    added database config

---
+++ _config/database.js
@@ -0,0 +1,45 @@
+/**
+*
+* Dependencies
+*
+**/
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+/**
+*
+* Database Connections
+* Requires connection variables from .env:
+* - DB_STRING
+* - DB_STRING_PROD
+**/
+// Connection strings
+const devConnection = process.env.DB_STRING;
+const prodConnection = process.env.DB_STRING_PROD;
+
+// Connect to the correct environment database
+if (process.env.NODE_ENV === 'production') {
+
+    // Mongoose connection to production string
+    mongoose.connect(prodConnection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    // When connection is successful
+    mongoose.connection.on('connected', () => {
+        console.log('Production database connected');
+    });
+
+} else {
+
+    // Mongoose connection to development string
+    mongoose.connect(devConnection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    // When connection is successful
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected');
+    });
+
+}
