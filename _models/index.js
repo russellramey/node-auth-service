f319ce83f1093728afd8e0425bba09444ca3b809
Russell Ramey <doublercreative@gmail.com>
2022-01-19 13:48:46 -0600

    updated models index

---
+++ _models/index.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6*/
+/**
+*
+* Dependencies
+*
+**/
+const User = require('./User')
+const Token = require('./Token')
+
+/**
+*
+* Export
+*
+**/
+module.exports.models = {
+	User,
+	Token
+};
