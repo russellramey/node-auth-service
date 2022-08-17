cbc67efc6a2efea158258338cacab59dd317ad63
Russell Ramey <doublercreative@gmail.com>
2022-08-17 09:18:46 -0500

    hardened user token middleware with extra prop checks

--- _config/passport-jwt.js
+++ _config/passport-jwt.js
@@ -7,6 +7,7 @@
 const passportjwt = require('passport-jwt');
 const users = require('../_models/User');
 const tokens = require('../_models/Token');
+const hash = require('../_utilities/hash');
 const fs = require('fs');
 require('dotenv').config();
 
@@ -35,18 +36,19 @@ module.exports = (passport) => {
     // The JWT payload is passed into the verify callback
     passport.use(new passportjwt.Strategy(options, async (jwt_payload, done) => {
 
-        // If jwt is expired
-        if(jwt_payload.exp < Date.now()) {
+        // If jwt is expired, or missing required params
+        if(jwt_payload.exp < Date.now() || !jwt_payload.jti || !jwt_payload.sub) {
             return done(null, false);
         }
 
         try{
             // Find token
             const token = await tokens.getTokens({ _id: jwt_payload.sub }, [], true);
-            // If no token, or token expired
-            if(!token || !token.user || token.revoked || (token.expires_at < Date.now())){
-                return done(null, false);
-            }
+
+            // If no token, or token revoked
+            if(!token || !token.user || token.revoked) return done(null, false);
+            // If token expired or no hash (JTI)
+            if((token.expires_at < Date.now()) ||  jwt_payload.jti !== hash.hashString(token.id.toString(), token.user.id.toString()).hash) return done(null, false);
 
             // Return user and token objects
             return done(null, { token });
